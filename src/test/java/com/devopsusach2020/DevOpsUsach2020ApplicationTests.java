899eae66e71de9c69a9ed40457e26a0ff2f68768
Samuel López M <samuel.lopez@redsalud.gov.cl>
2022-01-14 10:42:43 -0300

    Elimina código comentado en archivo de testing

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,6 +1,5 @@
 package com.devopsusach2020;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,7 +9,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import com.devopsusach2020.rest.RestData;
 
@@ -27,62 +25,21 @@ class DevOpsUsach2020ApplicationTests {
 	}
 
 	@Test
-	void noEncuentraPais() throws Exception{
-		//RestData r = new RestData();
-
+	void noEncuentraPais() throws Exception{		
 		this.mvc.perform(get("/estadoPais?pais=")).andExpect(status().isNotFound());
-
-
-		//assertThat(r.getTotalPais("")).;
-		
-		// MediaLibraries mediaLibraries = createMediaLibrariesObject();//creates mediaLibraries Object; code not shown
-        // given(libraryService.getAllLibraries(collectionId,offset,limit)).willReturn(mediaLibraries); //mocking the service
-
-        // mvc.perform(get("assetservice/rest/v1.0/libraries/search/?collectionIds=418A70D0F31010038152080020F03012&offset=0&limit=1"))
-
-        //         .andDo(print())
-        //         .andExpect(status().isOk());
 	}
 
 	@Test
 	void mensajePrueba() throws Exception{
 		RestData r = new RestData();
-
-		assertThat(r.getData("Hola Mundo")).isNotNull();
-
-		//this.mvc.perform(get("/estadoPais?pais=")).andExpect(status().isNotFound());
-
-
-		//assertThat(r.getTotalPais("")).;
-		
-		// MediaLibraries mediaLibraries = createMediaLibrariesObject();//creates mediaLibraries Object; code not shown
-        // given(libraryService.getAllLibraries(collectionId,offset,limit)).willReturn(mediaLibraries); //mocking the service
-
-        // mvc.perform(get("assetservice/rest/v1.0/libraries/search/?collectionIds=418A70D0F31010038152080020F03012&offset=0&limit=1"))
-
-        //         .andDo(print())
-        //         .andExpect(status().isOk());
+		assertThat(r.getData("Hola Mundo")).isNotNull();	
 	}
 
 
 	@Test
 	void datosPais() throws Exception{
 		RestData r = new RestData();
-
-		assertThat(r.getTotalPais("Chile")).isNotNull();
-
-		//this.mvc.perform(get("/estadoPais?pais=")).andExpect(status().isNotFound());
-
-
-		//assertThat(r.getTotalPais("")).;
-		
-		// MediaLibraries mediaLibraries = createMediaLibrariesObject();//creates mediaLibraries Object; code not shown
-        // given(libraryService.getAllLibraries(collectionId,offset,limit)).willReturn(mediaLibraries); //mocking the service
-
-        // mvc.perform(get("assetservice/rest/v1.0/libraries/search/?collectionIds=418A70D0F31010038152080020F03012&offset=0&limit=1"))
-
-        //         .andDo(print())
-        //         .andExpect(status().isOk());
+		assertThat(r.getTotalPais("Chile")).isNotNull();		
 	}
 
 }
